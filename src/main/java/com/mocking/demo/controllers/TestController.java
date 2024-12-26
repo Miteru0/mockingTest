ba666ebb418ad4c767d090df30a531c588d0c3a2
Miteru0 <miteruolen@gmail.com>
2024-12-26 03:07:46 +0100

    things added

---
+++ src/main/java/com/mocking/demo/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.mocking.demo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String getMethodName() {
+        return "Hello, it all works!";
+    }
+    
+
+}
